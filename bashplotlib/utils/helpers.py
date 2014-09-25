13870220aa6319be8d5ac036d1984c5165649f97
Brian McDonnell <mcdonnellb@gmail.com>
2014-09-25 23:32:14 +0100

    Fixed PEP8 violation: Should not assign to a lambda.

--- bashplotlib/utils/helpers.py
+++ bashplotlib/utils/helpers.py
@@ -7,7 +7,8 @@ Various helpful function for bashplotlib
 
 import sys
 
-isiterable = lambda x: hasattr(x, '__iter__') or hasattr(x, '__getitem__')
+def isiterable(x):
+    return hasattr(x, '__iter__') or hasattr(x, '__getitem__')
 
 bcolours = {
     "white":   '\033[97m',
